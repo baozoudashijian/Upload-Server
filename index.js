c438d6b411d26a671655bed4d0ad22e98a25b589
Roger Zhang <1787009521@qq.com>
2022-02-10 13:59:50 +0800

    feat：multer获取图片，并且保存起来

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const app = express()
 // url发送请求使用get
 // post
 // put
-app.post('/', function(req, res) {
+app.post('/', upload.single('avatar'), function(req, res) {
+  // 通过multer来拿到avatar, 并且会存到那个地址.
+  console.log(req.file)
   res.send('Hello World')
 })
 
