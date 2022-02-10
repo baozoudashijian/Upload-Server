d78f43d71e8744ed96761aef0a5b1519e0572044
Roger Zhang <1787009521@qq.com>
2022-02-10 14:06:58 +0800

    feat：加入上传页面

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express')
 const multer  = require('multer')
 const cors = require('cors')
 const upload =  multer({ dest: 'uploads/'})
+const path = require('path')
 
 const app = express()
 
+// 映射静态资源
+app.get('/index.html', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
 // url发送请求使用get
 // post
 // put
-app.post('/', upload.single('avatar'), function(req, res) {
+app.post('/upload', upload.single('avatar'), function(req, res) {
   // 通过multer来拿到avatar, 并且会存到那个地址.
   console.log(req.file)
   res.send('Hello World')
